4a119b1708bb38392ef48e40b4e20fc82b8c0b16
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-04-26 00:20:53 +0300

    guestbook fix

--- src/app/guestbook/page.jsx
+++ src/app/guestbook/page.jsx
@@ -1,21 +1,30 @@
-import Authentication from "@/components/guestbook/auth/Authentication";
-import Entries from "@/components/guestbook/Entries";
-import Notice from "@/components/guestbook/Notice";
+// import Authentication from "@/components/guestbook/auth/Authentication";
+// import Entries from "@/components/guestbook/Entries";
+// import Notice from "@/components/guestbook/Notice";
 
-export const metadata = {
-    title: "Guestbook - greg de Orayom",
-    description: "Software Engineer & UI/UX designer",
-};
+// export const metadata = {
+//     title: "Guestbook - greg de Orayom",
+//     description: "Software Engineer & UI/UX designer",
+// };
 
-export default function GuestBookPage() {
-    return (
-        <div className="flex flex-col space-y-6">
-            <h1 className="text-2xl font-semibold tracking-tighter">
-                Sign my guestbook
-            </h1>
-            <Notice />
-            <Authentication />
-            <Entries />
-        </div>
-    );
+// export default function GuestBookPage() {
+//     return (
+//         <div className="flex flex-col space-y-6">
+//             <h1 className="text-2xl font-semibold tracking-tighter">
+//                 Sign my guestbook
+//             </h1>
+//             <Notice />
+//             <Authentication />
+//             <Entries />
+//         </div>
+//     );
+// }
+import React from 'react'
+
+function page() {
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
