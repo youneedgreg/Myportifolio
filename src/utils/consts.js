d7f29815d56e6d154e6c69177cafebac120eb5ec
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-06-04 03:10:05 +0300

    Updated resume with new skills in artificial intelligence and machine learning, added recent projects, and refined work experience details. Also, uncommented navigation link for the work section in the project constants.

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -6,7 +6,7 @@ export const projectURL = "https://gregorytemwa.vercel.app/";
 
 export const NAVIGATION_LINKS = [
     { path: "/", label: "home" },
-    // { path: "/work", label: "work" },
+    { path: "/work", label: "work" },
     // { path: "/about", label: "about" },
     // { path: "/activity", label: "activity" },
     // { path: "/notes", label: "notes" },
