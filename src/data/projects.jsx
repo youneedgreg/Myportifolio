81ff56bb33528dafd9b873c843fa78345ac9ebad
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-06-04 03:07:27 +0300

    removed unecessary projects

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,21 +1,11 @@
 import autopromoph from "../../public/images/projects/auto-promo-ph.png";
 import sevendaysinn from "../../public/images/projects/sevendaysinn.png";
-import adminseven from 	"../../public/images/projects/adminseven.png";
+import adminseven from "../../public/images/projects/adminseven.png";
 import superafrica from "../../public/images/projects/superafrica.png";
 import saliksik from "../../public/images/projects/saliksik.png";
 import smpt from "../../public/images/projects/smpt.png";
-import adselle_shop from "../../public/images/projects/adselle-shop.png";
-import adselle from "../../public/images/projects/adselle.png";
 import timco_ph from "../../public/images/projects/timco-ph.png";
 import clafootwear from "../../public/images/projects/cla-footwear.png";
-import propertygo_rental from "../../public/images/projects/propertygo-rental.png";
-import transpopad_rental from "../../public/images/projects/transpopad-rental.png";
-import spare_ph from "../../public/images/projects/spare-ph.png";
-import buysellee_mobile from "../../public/images/projects/buysellee-mobile.png";
-import yellowpad_mobile from "../../public/images/projects/yellowpad-mobile.png";
-import transpopad_mobile from "../../public/images/projects/transpopad-mobile.png";
-import propertygo_mobile from "../../public/images/projects/propertygo-mobile.png";
-import adselle_mobile from "../../public/images/projects/adselle-mobile.png";
 
 const PROJECTS = [
     {
@@ -45,97 +35,6 @@ const PROJECTS = [
         webUrl: "https://superafricasafaris.com/",
         date: "2025",
     },
-    {
-        title: "Propertygo Rental",
-        company: "padtech",
-        type: "Web",
-        category: "Company",
-        image: propertygo_rental,
-        webUrl: "https://rent.propertygo.site/",
-        date: "2025",
-    },
-    {
-        title: "Adselle",
-        company: "padtech",
-        type: "Mobile",
-        category: "Company",
-        image: adselle_mobile,
-        androidUrl: "https://play.google.com/store/apps/details?id=com.adselle",
-        iosUrl: "https://apps.apple.com/gb/app/adselle/id1616805970?platform=iphone",
-        date: "2025",
-    },
-    {
-        title: "Propertygo",
-        company: "padtech",
-        type: "Mobile",
-        category: "Company",
-        image: propertygo_mobile,
-        androidUrl:
-            "https://play.google.com/store/apps/details?id=com.adselle.propertygo",
-        iosUrl: "https://apps.apple.com/gb/app/propertygo-real-estate-rentals/id6450562354?platform=iphone",
-        date: "2025",
-    },
-    {
-        title: "Transpopad",
-        company: "padtech",
-        type: "Mobile",
-        category: "Company",
-        image: transpopad_mobile,
-        androidUrl:
-            "https://play.google.com/store/apps/details?id=com.adselle.transpopad",
-        iosUrl: "https://apps.apple.com/gb/app/transpopad-buy-rent-cars/id6451374612?platform=iphone",
-        date: "2025",
-    },
-    {
-        title: "Yellowpad",
-        company: "padtech",
-        type: "Mobile",
-        category: "Company",
-        image: yellowpad_mobile,
-        androidUrl:
-            "https://play.google.com/store/apps/details?id=com.adselle.yellowpad",
-        iosUrl: "https://apps.apple.com/gb/app/yellowpad-business-services/id6466572081?platform=iphone",
-        date: "2025",
-    },
-    {
-        title: "Buysellee",
-        company: "padtech",
-        type: "Mobile",
-        category: "Company",
-        image: buysellee_mobile,
-        androidUrl:
-            "https://play.google.com/store/apps/details?id=com.adselle.buysellee",
-        iosUrl: "https://apps.apple.com/gb/app/buysellee-second-hand-shop/id6466649692?platform=iphone",
-        date: "2025",
-    },
-    {
-        title: "Spare PH",
-        company: "padtech",
-        type: "Web",
-        category: "Company",
-        image: spare_ph,
-        webUrl: "https://europemachineparts.com/",
-        date: "2024",
-    },
-    {
-        title: "Adselle Shop",
-        company: "padtech",
-        type: "Web",
-        category: "Company",
-        image: adselle_shop,
-        webUrl: "https://www.adselle.shop/",
-        date: "2024",
-    },
-    {
-        title: "Adselle National List",
-        company: "padtech",
-        type: "Web",
-        category: "Company",
-        image: adselle,
-        webUrl: "https://www.adselle.com/",
-        date: "2024",
-    },
-
     // === Freelance Projects ===
     {
         title: "TIMCO PH",
@@ -173,7 +72,6 @@ const PROJECTS = [
         webUrl: "https://auto-promo-ph.vercel.app/",
         date: "2023",
     },
-
     // === University Projects ===
     {
         title: "SALIKSIK: UPHSL Research Repository",
