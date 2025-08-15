7217a7e76a16c828701f8925d535c2ad85fcfeb8
Gregory Temwa <gregorytemwa1212@gmail.com>
2025-08-15 21:27:36 +0300

    redo of my portifolio

--- src/components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-const Card = ({ children }) => (
-    <div className="flex items-center justify-between rounded-sm border border-gray-200 p-3 shadow-xs hover:bg-slate-50 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:bg-neutral-800">
-        {children}
-    </div>
-);
-
-export default Card;
--- src/components/ui/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
-
-const Loading = () => (
-    <div className="flex items-center justify-center">
-        <AiOutlineLoading3Quarters className="animate-spin" />
-    </div>
-);
-
-export default Loading;
